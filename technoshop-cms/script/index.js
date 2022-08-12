10fe019ba197b886d7f1aabed937b2a28a9bbded
Kislitskaya_I <IKislitskaya@ibagroup.eu>
2022-08-12 14:44:00 +0300

    fix: error in index.js

--- technoshop-cms/script/index.js
+++ technoshop-cms/script/index.js
@@ -1,5 +1,5 @@
 import { modalBtn, modal } from "./elems.js";
-import { formController } from "./formConroller.js";
+import { formController } from "./formController.js";
 import { modalController } from "./modalController.js";
 import { previewController } from "./previewController.js";
 import { tableController } from "./tableController.js";
